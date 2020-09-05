7f2bd0545be65a7d09eda776c58e99dfaa1e9e6e
ni-k-azakov <nani.l.desu.l.ka@gmail.com>
2020-09-05 18:46:54 +0300

    :bug: Empty string bug fix

--- os_and_bash/lab_01/2.sh
+++ os_and_bash/lab_01/2.sh
@@ -1,9 +1,16 @@
 read input
-
+while [ -z $input ]
+do
+	read input
+done
 while [ $input != 'q' ]
 do
 	output+=$input
 	read input
+	while [ -z $input ]
+	do
+		read input
+	done
 done
 
 echo $output
